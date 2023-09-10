db9743fafee902bf0d7d6d2b54afaca11174c8f1
alexandrit178 <144226843+alexandrit178@users.noreply.github.com>
2023-09-10 23:17:09 +0300

    Add files via upload

---
+++ 3 (14).pas
@@ -0,0 +1,16 @@
+﻿program zad14; 
+var a, b, c: integer;
+begin
+  writeln('Введите 3 числа через пробел');
+  read(a, b, c);
+  if (a < b) and (a < c) and (b < c) Then
+    write(a, ' ', b, ' ', c)
+  else if (b < a) and (b < c) and (a < c) Then
+    write(b, ' ', a, ' ', c)
+  else if (c < a) and (c < b) and (a < b) Then
+    write(c, ' ', a, ' ', b)
+  else if (b < a) and (b < c) and (c < a) Then
+    write(b, ' ', c, ' ', a)
+  else
+    write(c, ' ', b, ' ', a)
+end.
---
+++ 4 (19).pas
@@ -0,0 +1,16 @@
+﻿program zad19; 
+var a, b, c, d, x: integer;
+begin
+writeln('введите четырёхзначное число');
+read (x);
+a:= x div 1000 ;
+b:= x mod 1000 div 100 ;
+c:= x mod 100 div 10;
+d:= x mod 10;
+if (a=d) and (b=c) then
+  writeln('число является палидромом ')
+else
+  writeln('число не является палидромом');
+end.
+
+
