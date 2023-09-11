b26d7821e5626d40fa2babe72b6e8aeb811274f9
alexandrit178 <144226843+alexandrit178@users.noreply.github.com>
2023-09-11 09:27:44 +0300

    Add files via upload

---
+++ 4 (25).pas
@@ -0,0 +1,43 @@
+﻿program zad25;
+
+var
+  a, b, c, d, e, f: integer;
+
+begin
+  
+  writeln('Введите цену товара ');
+  write('рубли  ');
+  readln(a);
+  write('копейки ');
+  readln(b);
+ 
+  writeln('Введите сумму, заплаченную за товар ');
+  write('рубли ');
+  readln(c);
+  write('копейки ');
+  readln(d);
+
+  // Вычисление сдачи
+  if c > a then
+  begin
+    e := c - a;
+    f := d - b;
+    if f < 0 then
+    begin
+      f := 100 + f;
+      e := e - 1;
+    end;
+  end
+  else
+  begin
+    e := 0;
+    f := 0;
+    writeln('Недостаточно средств для оплаты товара');
+    
+  end;
+
+ 
+  writeln('Сдача ');
+  writeln('рубли', e);
+  writeln('копейки ', f);
+end.
