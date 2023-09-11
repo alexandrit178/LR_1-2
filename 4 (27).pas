75b1b00cb25a4e0ceaefed39994076360b835392
alexandrit178 <144226843+alexandrit178@users.noreply.github.com>
2023-09-11 09:26:04 +0300

    Add files via upload

---
+++ 4 (27).pas
@@ -0,0 +1,29 @@
+﻿program zad27;
+
+var
+  a, b, c: integer;
+
+begin
+
+  writeln('Введите длину стороны a:');
+  readln(a);
+  writeln('Введите длину стороны b:');
+  readln(b);
+  writeln('Введите длину стороны c:');
+  readln(c);
+  
+  if (a + b > c) and (a + c > b) and (b + c > a) then
+  begin
+    
+    if (a * a + b * b = c * c) or (a * a + c * c = b * b) or (b * b + c * c = a * a) then
+      writeln('rectangular')//прямоугольный
+    
+    else if (a * a + b * b < c * c) or (a * a + c * c < b * b) or (b * b + c * c < a * a) then
+      writeln('acute')//острый
+    else
+      writeln('obtuse'); //тупой
+  end
+  else
+    writeln('impossible'); //не существует
+end.
+
